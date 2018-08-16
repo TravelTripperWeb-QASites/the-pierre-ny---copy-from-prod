d493ed921b3d6322217e02a159818d1318fd1c0d
dmanikym <dmanikyam@traveltripper.com>
2018-08-16 18:05:02 +0530

    Navigation in making

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,3 +1,10 @@
 /*
    Settings and other scripts
    ========================================================================== */
+
+// Bootstrap Select trigger
+$('.selectpicker')
+  .selectpicker({
+    style: 'btn-transparent',
+    size: 4
+  });
