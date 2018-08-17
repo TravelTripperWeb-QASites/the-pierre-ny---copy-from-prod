ecfa1fedae46a74ada43c1d76df611fae48884f8
dmanikym <dmanikyam@traveltripper.com>
2018-08-17 22:46:37 +0530

    Scroll Nav implementation

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,9 +2,62 @@
    Settings and other scripts
    ========================================================================== */
 
+navOnScroll(); //from function.js
+
 // Bootstrap Select trigger
 $('.selectpicker')
   .selectpicker({
     style: 'btn-transparent',
     size: 4
+  });
+
+//Main Nav Dropdown
+$(window)
+  .on("load", function () {
+    // nav menu on hover
+    $('ul.navbar-nav li.dropdown a')
+      .attr('data-toggle', 'disable');
+    if ($(window)
+      .width() > 1201) {
+      //Add Hover effect to menus
+      jQuery('ul.navbar-nav li.dropdown')
+        .hover(function () {
+          jQuery(this)
+            .find('.dropdown-menu')
+            .stop(true, true)
+            .delay(200)
+            .fadeIn();
+        }, function () {
+          jQuery(this)
+            .find('.dropdown-menu')
+            .stop(true, true)
+            .delay(200)
+            .fadeOut();
+        });
+    } else {
+      $('ul.navbar-nav li.dropdown')
+        .click(function (e) {
+          e.preventDefault();
+          jQuery(this)
+            .find('.dropdown-menu')
+            .fadeToggle();
+          $(this)
+            .find(".nav-link")
+            .toggleClass("open");
+        });
+      $('ul.navbar-nav li.dropdown a span')
+        .click(function () {
+          var link = jQuery(this)
+            .closest('a')
+            .attr("href");
+          window.location.href = link;
+        });
+      $('ul.navbar-nav li.dropdown .dropdown-item')
+        .click(function () {
+          var link = jQuery(this)
+            .closest('a')
+            .attr("href");
+          window.location.href = link;
+        });
+    }
   });
