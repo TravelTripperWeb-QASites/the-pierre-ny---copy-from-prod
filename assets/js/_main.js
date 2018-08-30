00d2a5b230b5e87fb7045c10988640821fb955f5
dmanikym <dmanikyam@traveltripper.com>
2018-08-30 19:00:14 +0530

    Merge fixes

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -13,52 +13,71 @@ $('.selectpicker')
 
 // Arrival and Departure datepicker
 
-$("#arrival_date").datepicker({
-          dateFormat: "yy-mm-dd",
-    altField  : '#arrival_date',
-    altFormat : 'yy-mm-dd',
-          minDate: 0,
-          onSelect: function (date) {
-              var date2 = $('#arrival_date').datepicker('getDate');
-              date2.setDate(date2.getDate() + 1);
-              $('#departure_date').datepicker('setDate', date2);
-              //sets minDate to dt1 date + 1
-              $('#departure_date').datepicker('option', 'minDate', date2);
-          }
-      });
-      $('#departure_date').datepicker({
-          dateFormat: "yy-mm-dd",
-    altField  : '#departure_dates',
-    altFormat : 'yy-mm-dd',
-          onClose: function () {
-              var dt1 = $('#v').datepicker('getDate');
-              console.log(dt1);
-              var dt2 = $('#departure_date').datepicker('getDate');
-              if (dt2 <= dt1) {
-                  var minDate = $('#departure_date').datepicker('option', 'minDate');
-                  $('#departure_date').datepicker('setDate', minDate);
-              }
-          }
-      });
+$("#arrival_date")
+  .datepicker({
+    dateFormat: "yy-mm-dd",
+    altField: '#arrival_date',
+    altFormat: 'yy-mm-dd',
+    minDate: 0,
+    onSelect: function (date) {
+      var date2 = $('#arrival_date')
+        .datepicker('getDate');
+      date2.setDate(date2.getDate() + 1);
+      $('#departure_date')
+        .datepicker('setDate', date2);
+      //sets minDate to dt1 date + 1
+      $('#departure_date')
+        .datepicker('option', 'minDate', date2);
+    }
+  });
+$('#departure_date')
+  .datepicker({
+    dateFormat: "yy-mm-dd",
+    altField: '#departure_dates',
+    altFormat: 'yy-mm-dd',
+    onClose: function () {
+      var dt1 = $('#v')
+        .datepicker('getDate');
+      console.log(dt1);
+      var dt2 = $('#departure_date')
+        .datepicker('getDate');
+      if (dt2 <= dt1) {
+        var minDate = $('#departure_date')
+          .datepicker('option', 'minDate');
+        $('#departure_date')
+          .datepicker('setDate', minDate);
+      }
+    }
+  });
 
 // Filter
 
-$(".filter").on("click", function() {
-        var $this = $(this);
-        // if we click the active tab, do nothing
-        if (!$this.hasClass("active")) {
-            $(".filter").removeClass("active");
-            $this.addClass("active"); // set the active tab
-            var $filter = $this.data("rel"); // get the data-rel value from selected tab and set as filter
-            $filter == 'all' ? // if we select "view all", return to initial settings and show all
-                $(".fancybox").attr("data-fancybox-group", "gallery").not(":visible").fadeIn()
+$(".filter")
+  .on("click", function () {
+    var $this = $(this);
+    // if we click the active tab, do nothing
+    if (!$this.hasClass("active")) {
+      $(".filter")
+        .removeClass("active");
+      $this.addClass("active"); // set the active tab
+      var $filter = $this.data("rel"); // get the data-rel value from selected tab and set as filter
+      $filter == 'all' ? // if we select "view all", return to initial settings and show all
+        $(".fancybox")
+        .attr("data-fancybox-group", "gallery")
+        .not(":visible")
+        .fadeIn()
 
-                : // otherwise
-                $(".fancybox").fadeOut(0).filter(function() {
-                    return $(this).data("filter") == $filter; // set data-filter value as the data-rel value of selected tab
-                }).attr("data-fancybox-group", $filter).fadeIn(1000); // set data-fancybox-group and show filtered elements
-        } // if
-    }); // on
+        : // otherwise
+        $(".fancybox")
+        .fadeOut(0)
+        .filter(function () {
+          return $(this)
+            .data("filter") == $filter; // set data-filter value as the data-rel value of selected tab
+        })
+        .attr("data-fancybox-group", $filter)
+        .fadeIn(1000); // set data-fancybox-group and show filtered elements
+    } // if
+  }); // on
 
 //Main Nav Dropdown
 $(window)
@@ -315,7 +334,6 @@ $(".gallery-wrapper .sub-nav li a")
         })
         .attr("data-fancybox-group", $filter)
         .fadeIn(1000); // set data-fancybox-group and show filtered elements
-      .fadeIn(1000); // set data-fancybox-group and show filtered elements
       //reset lightgallery after filtering
       setTimeout(function () {
         var lightgallery = $('[data-offergallery]');
